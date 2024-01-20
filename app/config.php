d2817eaeee1ce6d018c698d6e47beeceaede1c50
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-01-20 08:25:51 +0530

    Add files via upload

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+//Config Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'indianof_panel');
+define('DB_PASS', '@7802868506@');
+define('DB_NAME', 'indianof_smm');
+define('TIMEZONE', 'Aisa/Kolkata');
+define('ENCRYPTION_KEY', '1c8df2b6a9fc69744ce0fe6239e579ce');
